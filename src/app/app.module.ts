00e954ffc7ee132ee391ad5af92c8155b921cd01
cswebdev <csnider91@live.com>
2024-01-27 15:58:53 -0500

    adding assets and created a skills list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HeaderComponent
+    HeaderComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
